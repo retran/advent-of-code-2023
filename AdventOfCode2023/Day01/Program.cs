2bcbd4628f086d1139218be386a17f681380272c
Andrew Vasilyev <me@retran.me>
2023-12-03 19:16:36 +0100

    refactor day01 solutions

--- AdventOfCode2023/Day01/Program.cs
+++ AdventOfCode2023/Day01/Program.cs
@@ -6,26 +6,10 @@ void Part01()
 {
     var lines = File.ReadAllLines("calibration.txt");
     var sum = 0;
-
     foreach (var line in lines)
     {
-        var firstDigit = "";
-        var lastDigit = "";
-
-        for (var i = 0; i < line.Length; i++)
-            if (char.IsDigit(line[i]))
-            {
-                firstDigit = line[i].ToString();
-                break;
-            }
-
-        for (var i = line.Length - 1; i >= 0; i--)
-            if (char.IsDigit(line[i]))
-            {
-                lastDigit = line[i].ToString();
-                break;
-            }
-
+        var firstDigit = FindFirstDigit(line);
+        var lastDigit = FindLastDigit(line);
         if (!string.IsNullOrEmpty(firstDigit) && !string.IsNullOrEmpty(lastDigit))
             sum += int.Parse(firstDigit + lastDigit);
     }
@@ -47,61 +31,64 @@ void Part02()
         { "eight", 8 },
         { "nine", 9 }
     };
-
     var lines = File.ReadAllLines("calibration.txt");
     var sum = 0;
-
     foreach (var line in lines)
     {
-        var firstDigit = "";
-        var lastDigit = "";
-
-        for (var i = 0; i < line.Length; i++)
-        {
-            if (char.IsDigit(line[i]))
-            {
-                firstDigit = line[i].ToString();
-                break;
-            }
-
-            foreach (var spelledOutDigit in spelledOutDigits)
-                if (i + spelledOutDigit.Key.Length <= line.Length &&
-                    line.Substring(i, spelledOutDigit.Key.Length) == spelledOutDigit.Key)
-                {
-                    firstDigit = spelledOutDigit.Value.ToString();
-                    break;
-                }
-
-            if (!string.IsNullOrEmpty(firstDigit))
-                break;
-        }
-
-        for (var i = line.Length - 1; i >= 0; i--)
-        {
-            if (char.IsDigit(line[i]))
-            {
-                lastDigit = line[i].ToString();
-                break;
-            }
-
-            foreach (var spelledOutDigit in spelledOutDigits)
-                if (i - spelledOutDigit.Key.Length + 1 >= 0 &&
-                    line.Substring(i - spelledOutDigit.Key.Length + 1, spelledOutDigit.Key.Length) ==
-                    spelledOutDigit.Key)
-                {
-                    lastDigit = spelledOutDigit.Value.ToString();
-                    break;
-                }
-
-            if (!string.IsNullOrEmpty(lastDigit))
-                break;
-        }
-
+        var firstDigit = FindFirstDigitOrSpelledOutDigit(line, spelledOutDigits);
+        var lastDigit = FindLastDigitOrSpelledOutDigit(line, spelledOutDigits);
         if (!string.IsNullOrEmpty(firstDigit) && !string.IsNullOrEmpty(lastDigit))
             sum += int.Parse(firstDigit + lastDigit);
     }
 
     Console.WriteLine("The sum of all calibration values is: " + sum);
+}
+
+string FindFirstDigit(string line)
+{
+    for (var i = 0; i < line.Length; i++)
+        if (char.IsDigit(line[i]))
+            return line[i].ToString();
+    return "";
+}
+
+string FindLastDigit(string line)
+{
+    for (var i = line.Length - 1; i >= 0; i--)
+        if (char.IsDigit(line[i]))
+            return line[i].ToString();
+    return "";
+}
+
+string FindFirstDigitOrSpelledOutDigit(string line, Dictionary<string, int> spelledOutDigits)
+{
+    for (var i = 0; i < line.Length; i++)
+    {
+        if (char.IsDigit(line[i]))
+            return line[i].ToString();
+        foreach (var spelledOutDigit in spelledOutDigits)
+            if (i + spelledOutDigit.Key.Length <= line.Length &&
+                line.Substring(i, spelledOutDigit.Key.Length) == spelledOutDigit.Key)
+                return spelledOutDigit.Value.ToString();
+    }
+
+    return "";
+}
+
+string FindLastDigitOrSpelledOutDigit(string line, Dictionary<string, int> spelledOutDigits)
+{
+    for (var i = line.Length - 1; i >= 0; i--)
+    {
+        if (char.IsDigit(line[i]))
+            return line[i].ToString();
+        foreach (var spelledOutDigit in spelledOutDigits)
+            if (i - spelledOutDigit.Key.Length + 1 >= 0 &&
+                line.Substring(i - spelledOutDigit.Key.Length + 1, spelledOutDigit.Key.Length) ==
+                spelledOutDigit.Key)
+                return spelledOutDigit.Value.ToString();
+    }
+
+    return "";
 }
 
 Part01();
